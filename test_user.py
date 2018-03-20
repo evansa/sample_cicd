4c718981b7a1303d49c0773fe8a08bda04b44d4a
evansa <fiveholiday55@hotmail.com>
2018-03-20 17:19:09 +0000

    fixing invalid method signature

--- test_user.py
+++ test_user.py
@@ -1,15 +1,13 @@
 import pytest
 from user import User
  
-class TestUser:
+@pytest.fixture
+def new_user():
+    return User(firstname="Evans", lastname="Anyokwu",
+               job="Engineer", salary=500, address="Canvey Island")
 
-    @pytest.fixture
-    def new_user():
-        return User(firstname="Evans", lastname="Anyokwu",
-                    job="Engineer", salary=500, address="Canvey Island")
-
-    def test_create_user(new_user):                      
-        assert new_user.user_name == "Evans Anyokwu"
+def test_create_user(new_user):                      
+    assert new_user.user_name == "Evans Anyokwu"
         
-    def test_salary():
-        assert new_user.salary == 500 
+def test_salary():
+    assert new_user.salary == 500 
