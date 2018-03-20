7111472ef3b65fb6ff395eba9f45f0ed2833577d
Evans Anyokwu <evansa@hbc.co.uk>
2018-03-20 19:26:01 +0000

    added test for job title

--- test_user.py
+++ test_user.py
@@ -10,7 +10,7 @@ def test_create_user(new_user):
     assert new_user.user_name == "Evans Anyokwu"
         
 def test_job_name(new_user):
-	assert "Engineer" in new_user.user.job
+	assert "Engineer" in new_user.user_job
 	
 def test_get_salary(new_user):
     assert new_user.get_salary == 500 
