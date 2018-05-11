a132b710ec13a3577f082db912871f19a9ca3232
Evans A <fiveholiday55@hotmail.com>
2018-05-11 11:33:48 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 class User:
   '''
   just adding a simple comment 11/05/2018 
-  configure webhook.. again
+  configure webhook.. again2
   '''
   def __init__(self, firstname, lastname, job, salary, address):
     self.firstname = firstname
